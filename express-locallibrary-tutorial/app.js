5542dae1f86729a3da9a256003ec3c90db579899
JoshRK <josh.khez@gmail.com>
2023-12-10 02:45:28 +0100

    Ajout du projet

---
+++ express-locallibrary-tutorial/app.js
@@ -0,0 +1,62 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const wiki = require("./wiki.js");
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+
+const app = express();
+
+
+// On utilise pug
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/wiki", wiki);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+
+console.log(mongoose.connection.readyState);
+
+mongoose.set("strictQuery", false);
+const mongoDB = 'mongodb://localhost:27017';
+
+console.log("ici");
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+
+
+module.exports = app;
